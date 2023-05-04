4e7c61ae7637f13864387e532283c25c4d68e477
Replit user <>
2023-05-04 16:41:13 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -47,7 +47,7 @@ int is_valid(Node* n){
 
   for(int i = 0; i < 9; i++){
     for(int j  =0; j < 9; j++){
-
+      if(n->sudo[i][j] == 0) continue;
       for(int x = 0; x < 9; x++){
         if(x!=i){
           if(n->sudo[i][j] == n->sudo[x][j]) return 0;
